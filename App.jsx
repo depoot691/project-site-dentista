f4653baf84e4a75ffe26ffe80690ade0b1d34c21
Nathan <kakakanatha@gmail.com>
2026-05-18 20:47:42 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import banner from './assets/dentista.png'
 import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,25 +13,30 @@ function App() {
 
 return (
   <>
-      <div className='container py-4'>
-        <h1 className="text-primary">Dentista</h1>
 
-        <button className='btn btn-success mt-3'>clique aq</button>
-      </div>
+  <div className="container-fluid p-0">
+    <img 
+        src={banner}
+        className="img-fluid w-100"
+        alt="banner"
+        style={{ height: "500px", objectFit: "cover" }}
+    />
+  </div>
 
    <div className="d-flex justify-content-center align-items-center min-vh-100">
     <form className='w-50 mx-3'>
       <div className="mb-3">
         <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+          Email 
         </label>
         <input
           type="email"
           className="form-control"
           id="exampleInputEmail1"
+          placeholder='digite seu email'
         />
         <div className="form-text">
-          We'll never share your email with anyone else.
+          Nunca compartilharemos seu email com mais ninguém.
         </div>
       </div>
 
@@ -42,19 +48,11 @@ return (
           type="password"
           className="form-control"
           id="exampleInputPassword1"
+          placeholder='digite sua senha'
         />
       </div>
 
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          lembre-me
-        </label>
-      </div>
+    
 
       <button type="submit" className="btn btn-primary">
         enviar
