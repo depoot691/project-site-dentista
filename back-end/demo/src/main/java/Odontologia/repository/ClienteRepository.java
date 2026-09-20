10e5ae37c327d5a4c6194dd6cd79acb4b5d89da3
Nathan <kakakanatha@gmail.com>
2026-09-20 18:11:55 -0300

    Delete back-end/demo directory

--- back-end/demo/src/main/java/Odontologia/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package Odontologia.repository;
-
-import Odontologia.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository <Cliente, Integer> {
-}
--- back-end/demo/src/main/java/Odontologia/repository/DentistaRepository.java
+++
@@ -1,9 +0,0 @@
-package Odontologia.repository;
-
-import Odontologia.entity.Dentista;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DentistaRepository extends JpaRepository<Dentista, Integer> {
-}
